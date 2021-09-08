e42fe47c0f9a95317dcc5399e2b55465313c3706
Cole Whitman <colewhitman91@gmail.com>
2021-09-08 13:08:49 -0400

    added testimonial section

---
+++ script.js
@@ -0,0 +1,38 @@
+const testimonial = document.querySelector('.testimonial')
+const customerName = document.querySelector('.customer-name')
+
+const testimonials = [
+  {
+    name: '-Doug W',
+    text:
+      "\"Very conscientious and leaves the yard looking picture perfect. Also does a great fall clean up!\"",
+  },
+  {
+    name: '-Margaret M',
+    text:
+      "\"Dan is a thoughtful and kind man, as well as a darn good tender of the earth. I love that he helps me tend to my yard each year.\"",
+  },
+  {
+    name: '-Brendan J',
+    text:
+      "\"Great customer service and friendly, knowledgeable service staff.\"",
+  },
+  
+]
+
+let idx = 1
+
+function updateTestimonial() {
+  const { name, text } = testimonials[idx]
+
+  testimonial.innerHTML = text
+  customerName.innerHTML = name
+
+  idx++
+
+  if (idx > testimonials.length - 1) {
+    idx = 0
+  }
+}
+
+setInterval(updateTestimonial, 10000)
