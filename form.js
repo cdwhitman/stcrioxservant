9bd43ef5ce192803a38ca8c7067eb0da66036165
Cole Whitman <colewhitman91@gmail.com>
2021-09-28 16:11:56 -0400

    Updated testimonial switcher script

---
+++ form.js
@@ -0,0 +1,28 @@
+// form script
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    form.reset()
+    status.innerHTML = "Your Request Has Been Sent!";
+    status.classList.add("success");
+    setTimeout(function() { 
+      status.classList.remove("success");      
+    }, 4000);
+    setTimeout(function() { 
+      status.innerHTML = "Get Your Free Quote!";      
+    }, 4000);
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
--- script.js
+++ script.js
@@ -10,6 +10,11 @@ const testimonials = [
       "\"Very conscientious and leaves the yard looking picture perfect. Also does a great fall clean up!\"",
   },
   {
+    name: '-Wally R',
+    text:
+      "\"Dan and his team have been awesome! Very professional and easy to work with!\"",
+  },
+  {
     name: '-Margaret M',
     text:
       "\"Dan is a thoughtful and kind man, as well as a darn good tender of the earth. I love that he helps me tend to my yard each year.\"",
@@ -34,31 +39,3 @@ function updateTestimonial() {
 setInterval(updateTestimonial, 10000)
 
 
-// form script
-var form = document.getElementById("my-form");
-    
-async function handleSubmit(event) {
-  event.preventDefault();
-  var status = document.getElementById("my-form-status");
-  var data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-        'Accept': 'application/json'
-    }
-  }).then(response => {
-    form.reset()
-    status.innerHTML = "Your Request Has Been Sent!";
-    status.classList.add("success");
-    setTimeout(function() { 
-      status.classList.remove("success");      
-    }, 4000);
-    setTimeout(function() { 
-      status.innerHTML = "Get Your Free Quote!";      
-    }, 4000);
-  }).catch(error => {
-    status.innerHTML = "Oops! There was a problem submitting your form"
-  });
-}
-form.addEventListener("submit", handleSubmit)
