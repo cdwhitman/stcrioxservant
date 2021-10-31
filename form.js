7689b7b7db3f6f8a7eaa62c24e584c7389fdd996
Cole Whitman <colewhitman91@gmail.com>
2021-10-31 15:05:25 -0400

    added title to quote form

--- form.js
+++ form.js
@@ -1,28 +1,30 @@
 // form script
-var form = document.getElementById("my-form");
-    
+var form = document.getElementById('my-form');
+
 async function handleSubmit(event) {
   event.preventDefault();
-  var status = document.getElementById("my-form-status");
+  var status = document.getElementById('my-form-status');
   var data = new FormData(event.target);
   fetch(event.target.action, {
     method: form.method,
     body: data,
     headers: {
-        'Accept': 'application/json'
+      Accept: 'application/json'
     }
-  }).then(response => {
-    form.reset()
-    status.innerHTML = "Your Request Has Been Sent!";
-    status.classList.add("success");
-    setTimeout(function() { 
-      status.classList.remove("success");      
-    }, 4000);
-    setTimeout(function() { 
-      status.innerHTML = "Get Your Free Quote!";      
-    }, 4000);
-  }).catch(error => {
-    status.innerHTML = "Oops! There was a problem submitting your form"
-  });
+  })
+    .then((response) => {
+      form.reset();
+      status.innerHTML = 'Your Request Has Been Sent!';
+      status.classList.add('success');
+      setTimeout(function () {
+        status.classList.remove('success');
+      }, 4000);
+      setTimeout(function () {
+        status.innerHTML = 'Get Your Free Quote!';
+      }, 4000);
+    })
+    .catch((error) => {
+      status.innerHTML = 'There was a problem submitting your form';
+    });
 }
-form.addEventListener("submit", handleSubmit)
+form.addEventListener('submit', handleSubmit);
